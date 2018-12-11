38e941ad1db9b05dc1fda88ef1ad251d77a3b831
Kostas Liberopoulos <libekonst@gmail.com>
2018-12-11 20:37:36 +0200

    Add grid view.

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,40 @@ import './App.css';
 import { RadioCard } from './components/RadioCard';
 import { withRoot } from './withRoot';
 import store from './store';
+import {
+  GridList,
+  GridListTile,
+  GridListTileBar,
+  IconButton,
+  Grid,
+} from '@material-ui/core';
+import InfoIcon from '@material-ui/icons/Info';
 
 class App extends Component {
   render() {
     return (
-      <div style={{ padding: 20, display: 'flex', }}>
+      // <div
+      //   style={{
+      //     padding: 20,
+      //     display: 'flex',
+      //     flexWrap: 'wrap',
+      //     flexDirection: 'row',
+      //     justifyContent: 'spaceAround',
+      //     alignItems: 'center',
+      //   }}
+      // >
+      <Grid container spacing={16} justify="space-around">
         {store.map(radio => (
-          <div style={{padding: 10}}>
-          <RadioCard
-            source={radio.source}
-            key={radio.id}
-            img={radio.img}
-            name={radio.name}
-          />
-          </div>
+          <Grid item xs key={radio.id}>
+            <RadioCard
+              source={radio.source}
+              image={radio.image}
+              name={radio.name}
+              label={radio.label}
+            />
+          </Grid>
         ))}
-      </div>
+      </Grid>
     );
   }
 }
--- src/components/RadioCard.tsx
+++ src/components/RadioCard.tsx
@@ -1,46 +1,60 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
-import {
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  Typography,
-  IconButton,
-  CardActions,
-} from '@material-ui/core';
+import { CardActionArea, Typography, IconButton } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import { RadioTypes } from '../types';
 type State = {
   favorited: boolean;
 };
 
-type Props = {
-  name: string;
-  source: string;
-  img: string;
-}
-
-export class RadioCard extends Component<Props, State> {
+export class RadioCard extends Component<RadioTypes.Labelled, State> {
   state = {
     favorited: false,
   };
   addFavorite = () => this.setState(state => ({ favorited: !state.favorited }));
 
   render() {
-    const { img, name } = this.props;
+    const { image, name } = this.props;
     return (
-      <Card style={{ maxWidth: 200, minWidth: 150 }}>
-        <CardActionArea>
-          <CardMedia image={img} title={name} style={{ height: 200, width: 200}} />
+      <Card
+        style={{
+          // maxWidth: 200,
+          // minWidth: 200,
+          width: 200,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <CardActionArea /* style={{ height: 200 }} */>
+          <div
+            // src={image}
+            title={name}
+            style={{
+              backgroundImage: `url(${image})`,
+              height: 200,
+              width: 'auto',
+              backgroundSize: 'cover',
+              display: 'block',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+            }}
+          />
         </CardActionArea>
-        <CardContent style={{ display: 'flex', flexDirection: 'row', justifyContent: 'spaceBetween', alignContent: 'center' }}>
-          <Typography variant="body1">{name}</Typography>
-          {/* <CardActions> */}
-            <IconButton aria-label="Add to favorites" onClick={this.addFavorite}>
-              <FavoriteIcon color={this.state.favorited ? 'secondary' : undefined} />
-            </IconButton>
-          {/* </CardActions> */}
-        </CardContent>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="body1" style={{ padding: 12 }}>
+            {name}
+          </Typography>
+          <IconButton aria-label="Add to favorites" onClick={this.addFavorite}>
+            <FavoriteIcon color={this.state.favorited ? 'secondary' : undefined} />
+          </IconButton>
+        </div>
       </Card>
     );
   }
