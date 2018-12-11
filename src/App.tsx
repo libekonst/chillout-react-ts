268e43a06b9aae08082860df9b3d7f2e07544b8c
Kostas Liberopoulos <libekonst@gmail.com>
2018-12-11 02:27:28 +0200

    Set up basic theme settings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
 import './App.css';
+import { CssBaseline } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: pink,
+    type: 'light',
+  },
+  typography: {
+    useNextVariants: true,
+},
+});
 
 class App extends Component {
   public render() {
-    return <div />;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <div />;
+      </MuiThemeProvider>
+    );
   }
 }
 
